7fc3766d705da8558a127c5f550ab46708c57c16
Cristopher <cristopher.rexx@gmail.com>
2026-03-11 12:35:06 -0600

    Add Histogram

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
+import streamlit as st
 import pandas as pd
 import plotly.express as px
-import streamlit as st
 
-vehicles_data = pd.read_csv("vehicles_us.csv")
-
+# Leer el dataset
+df = pd.read_csv("vehicles_us.csv")
 
 # Encabezado de la aplicación
 st.header("Análisis Exploratorio de Datos de Vehículos")
 
 # Botón para construir un histograma
-if st.button("Construir Histograma"):
+if st.button("Crear Histograma"):
     st.write("Histograma de la columna 'clock_speed'")
-    if 'clock_speed' in vehicles_data.columns:
-        fig = px.histogram(vehicles_data, x='clock_speed', title='Histograma de Velocidad del Reloj')
+    if 'clock_speed' in df.columns:
+        fig = px.histogram(df, x='clock_speed', title='Histograma de Velocidad del Reloj')
     else:
-        fig = px.histogram(vehicles_data, x=vehicles_data.columns[0], title=f'Histograma de {vehicles_data.columns[0]}')
-    fig.show()
+        fig = px.histogram(df, x=df.columns[0], title=f'Histograma de {df.columns[0]}')
     st.plotly_chart(fig, use_container_width=True)
-
-# Botón para construir un gráfico de dispersión
-if st.button("Construir Scatter Plot"):
-    st.write("Creando Dispersion")
-    fig = px.scatter(vehicles_data, x="odometer", y="price") # crear un gráfico de dispersión
-    fig.show() # crear gráfico de dispersión
