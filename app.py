1af176c4b916ef3e33e224837e30e191f80608f4
Cristopher <cristopher.rexx@gmail.com>
2026-03-14 00:19:39 -0600

    Update Hist

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ st.header("Análisis Exploratorio de Datos de Vehículos")
 
 # Botón para construir un histograma
 if st.button("Construir Histograma"):
-    st.write("Histograma de la columna 'clock_speed'")
+    st.write("Construyendo Histograma'")
     if 'clock_speed' in vehicles_data.columns:
+        st.write("Columna 'clock_speed'")
         fig = px.histogram(vehicles_data, x='clock_speed', title='Histograma de Velocidad del Reloj')
     else:
+        st.write(f"Columna {vehicles_data.columns[0]}")
         fig = px.histogram(vehicles_data, x=vehicles_data.columns[0], title=f'Histograma de {vehicles_data.columns[0]}')
     fig.show()
     st.plotly_chart(fig, use_container_width=True)
